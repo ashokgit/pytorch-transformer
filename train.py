34c6b4c477056719b65b249c7506da33c346029d
Ashok Poudel <ashok.poudel@gmail.com>
2024-01-17 00:37:26 +0545

    Update train.py

--- train.py
+++ train.py
@@ -123,7 +123,10 @@ def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, 
 
 def get_all_sentences(ds, lang):
     for item in ds:
-        yield item['translation'][lang]
+        if lang == config['lang_src']:
+            yield item['en'] 
+        elif lang == config['lang_tgt']:
+            yield item['np']
 
 def get_or_build_tokenizer(config, ds, lang):
     tokenizer_path = Path(config['tokenizer_file'].format(lang))
